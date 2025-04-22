13108866446d4a3c5c54f57f9f174d6c43a422aa
DalaScript <nik.dalalishvili@gmail.com>
2025-04-22 09:23:13 +0400

    added check and middleware for routes

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
-
+import { user } from "@prisma/client"
+import { Request, Response, NextFunction } from "express";
 
 export const hashPassword = (password: string) => {
     return bcrypt.hash(password, 10);
@@ -21,10 +22,42 @@ export const createJWT = (user: {
         },
         process.env.JWT_SECRET
     );
-
     return token;
 }
 
 export const comparePasswords = (password: string, hash: string) => {
     return bcrypt.compare(password, hash);
+}
+
+declare module "express-serve-static-core" {
+    interface Request {
+        user: user;
+    }
+}
+
+export const protect = (req: Request, res: Response, next: NextFunction) => {
+    const bearer = req.headers.authorization;
+
+    if(!bearer) {
+        res.status(401).json({ message: "Not authorized" });
+        return;
+    }
+
+    const [, token] = bearer.split(" ");
+
+    try {
+        if(!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET is not defined");
+        }
+
+        const user = jwt.verify(token, process.env.JWT_SECRET) as user;
+        req.user = user;
+        next();
+    }catch(err) {
+        if(err instanceof jwt.JsonWebTokenError) {
+            res.status(401).json({ message: "Not a Valid Token Provided"});
+        }
+        res.status(500).json({ message: "Internal Server Error" });
+        return;
+    }
 }
